5bfc16c7f14a57b3da6745e5b024c3951818f657
lyuehh <lyuehh@gmail.com>
2015-07-15 14:58:22 +0800

    remove common test file

---
+++ src/alt.js
@@ -0,0 +1,8 @@
+var Alt = require('alt')
+var alt = new Alt()
+
+alt.dispatcher.register(console.log.bind(console))
+window.Alt = Alt
+
+module.exports = alt
+
--- src/main.js
+++ src/main.js
@@ -1,25 +1,36 @@
-/**
- * App entry point
- */
 
 import 'babel-core/polyfill';
 import React from 'react';
-import Router from 'react-router';
+import { Router, Route, Link } from 'react-router';
+import { history } from 'react-router/lib/HashHistory';
 
-import App from './app'
 import Index from './routers/index'
 import About from './routers/about'
 
-var Route = Router.Route;
-
-var routes = (
-  <Route path="/" handler={App}>
-    <Route path="about" handler={About}/>
-    <Route path="index" handler={Index}/>
-  </Route>
-);
-
-Router.run(routes, Router.HashLocation, (Root) => {
-  React.render(<Root/>, document.body);
+var App = React.createClass({
+  render() {
+    return (
+      <div>
+        <h1>App</h1>
+        {/* change the <a>s to <Links>s */}
+        <ul>
+          <li><Link to="/about">About</Link></li>
+          <li><Link to="/index">Index</Link></li>
+        </ul>
+        {this.props.children}
+      </div>
+    )
+  }
 });
+
+// Finally we render a `Router` component with some `Route`s, it'll do all
+// the fancy routing stuff for us.
+React.render((
+  <Router history={history}>
+    <Route path="/" component={App}>
+      <Route path="about" component={About}/>
+      <Route path="index" component={Index}/>
+    </Route>
+  </Router>
+), document.body);
 
